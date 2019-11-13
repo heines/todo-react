cebfd50018ace56793a153931f9a00c2df3fb272
清水園子 <s-shimizu@tsumikiinc.com>
2019-11-13 19:29:27 +0900

    文字盤設定。スタイル調整

--- todo-react/src/components/Clock.js
+++ todo-react/src/components/Clock.js
@@ -61,8 +61,14 @@ export default class Clock extends React.Component {
   }
 
   render() {
+    let list = [];
+    for(var i=1; i<13; i++) {
+      list.push(<div className={`Clock__${i}`}>{i}</div>);
+    }
+    console.log(list);
     return (
       <div className="Clock">
+        {list}
         <MovementHours date={this.state.date} />
         <MovementMinutes date={this.state.date} />
         <MovementSeconds date={this.state.date} />
