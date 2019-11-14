fd436223c590e512762e77cb9d98cfecd7608e1d
清水ハイネ <heine.shimizu@gmail.com>
2019-11-15 00:55:41 +0900

    フォームにクラス追加

--- todo-react/src/components/Todo.js
+++ todo-react/src/components/Todo.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import COLOR from './_const/COLOR';
+import styled from 'styled-components';
 
-const Todo = ({ onClick, completed, text }) => (
-  <li
+const Todo = styled.li`
+  font-family: monospace;
+  cursor: pointer;
+  font-size: 2em;
+  line-height: 1.5;
+  &.completed {
+    text-decoration:  line-through;
+    color: ${COLOR.BASE};
+  }
+`;
+
+const StyledTodo = ({ onClick, completed, text }) => (
+  <Todo
     onClick={onClick}
-    className={classNames(
-        "todo-item__text",
-        {"todo-item__text--completed" : completed}
-    )}
+    className={completed ? "completed" : ''}
   >
-    {completed ? "✔︎" : "◻️"}{" "}
+    {completed ? "✔︎" : "・"}{" "}
     <span>
       {text}
     </span>
-  </li>
+  </Todo>
 );
 
-Todo.propsTypes = {
+StyledTodo.propsTypes = {
   onClick: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
 }
 
-export default Todo;
+export default StyledTodo;
--- todo-react/src/containers/AddTodo.js
+++ todo-react/src/containers/AddTodo.js
@@ -2,12 +2,33 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addTodo } from '../actions';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import COLOR from '../components/_const/COLOR';
 
-const AddTodo = ({ dispatch }) => {
+const AddTodo = styled.form`
+  padding-top: 1.1em;
+  input {
+    outline: none;
+    font-size: 1.5em;
+  }
+  button {
+    font-size: 1.5em;
+    margin-left: .4em;
+    padding: 0.2em 0.45em;
+    color: ${COLOR.TEXT2};
+    background: ${COLOR.SUB};
+    border-radius: 0.15em;
+    box-sizing: border-box;
+    outline: none;
+    cursor: pointer;
+  }
+`;
+
+const StyledAddTodo = ({ dispatch }) => {
   let input;
   return (
     <div>
-      <form onSubmit={
+      <AddTodo onSubmit={
         (event) => {
           // React では false を返してもデフォルトの動作を抑止することができません。明示的に preventDefault を呼び出す必要があります。
           // from https://ja.reactjs.org/docs/handling-events.html
@@ -25,13 +46,13 @@ const AddTodo = ({ dispatch }) => {
         <button type="submit">
           Add Todo
         </button>
-      </form>
+      </AddTodo>
     </div>
   );
 };
 
-AddTodo.propTypes = {
+StyledAddTodo.propTypes = {
   dispatch: PropTypes.func.isRequired
 };
 
-export default connect()(AddTodo);
+export default connect()(StyledAddTodo);
